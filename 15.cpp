2ff8be3664a9968496a179f85e13b6dff2c1ed09
piyushmvs666-code <piyushmvs666@gmail.com>
2026-09-04 00:39:02 +0530

    .

---
+++ 15.cpp
@@ -0,0 +1,27 @@
+// swap 2 numbers.
+#include <iostream>
+using namespace std;
+
+
+void swap(int &a, int &b){
+
+    int c;
+    c=a;
+    a=b;
+    b=c;
+   
+}
+
+
+
+
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+
+    swap(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+    
+}
